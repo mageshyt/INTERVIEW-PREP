d20bb6b372868870eb4ad5a05be9f4545befd5bb
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-10-30 20:52:51 +0530

    modified union find tmeplate

--- Algorithms/unionFind/Template.py
+++ Algorithms/unionFind/Template.py
@@ -3,12 +3,12 @@ class UnionFind:
         self.parent = {}
 
     def find(self, x: int) -> int:
-        self.parent.setdefault(x, x)
+        self.parent.setdefault(x, x) # initialize the parent of the node to itself
 
-        if x != self.parent[x]:
+        if x != self.parent[x]: # if the node is not the parent of itself
             self.parent[x] = self.find(self.parent[x])
 
-        return self.parent[x]
+        return self.parent[x] # return the parent of the node
 
 
     def union(self, x: int, y: int) -> None:
