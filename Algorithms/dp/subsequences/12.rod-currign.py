1904e5c39ec8d8ee786dca2c5cf61a58233a28c2
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-11-28 11:48:19 +0530

    rod cutting space optmized added

--- Algorithms/dp/subsequences/12.rod-currign.py
+++ Algorithms/dp/subsequences/12.rod-currign.py
@@ -5,10 +5,7 @@ class Solution:
 
         def dfs(idx,rod_len):
             if idx==0:
-                if (rod_len) % (idx+1)==0:
-                    return price[idx] * (rod_len//(idx+1))
-
-                return 0
+                return price[idx] * (rod_len//(idx+1))
 
 
             if(idx,rod_len) in dp:
@@ -28,9 +25,20 @@ class Solution:
             return dp[(idx,rod_len)]
 
         return dfs(n-1,n)
+    # SPACE OPTIMIZED
+    def cutRod2(self, price, n):
+        dp=[0] * (n+1)
 
+        for i in range(1,n+1):
+            for j in range(i,n+1):
+                dp[j]=max(dp[j],price[i-1]+dp[j-i])
+
+        return dp[n]
 s=Solution()
 print(s.cutRod([1,5,8,9,10,17,17,20],8))
 print(s.cutRod([3,5,8,9,10,17,17,20],8))
 
+print("================================")
 
+print(s.cutRod2([1,5,8,9,10,17,17,20],8))
+print(s.cutRod2([3,5,8,9,10,17,17,20],8))
