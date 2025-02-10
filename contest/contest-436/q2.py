6502bdbd5abe36f022d36c3d8a48afc422f20216
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2025-02-10 23:37:36 +0530

    contest 436 optimzed solution added for q2

--- contest/contest-436/q2.py
+++ contest/contest-436/q2.py
@@ -1,4 +1,3 @@
-
 from typing import List
 import math
 class Solution:
@@ -9,28 +8,19 @@ class Solution:
             if ele not in min_idxMap:
                 min_idxMap[ele]=idx
 
-        res = [-1]*len(groups)
+        res = []
 
-        def helper(num):
-            best_divisor=float('inf')
-            for d in range(1,math.isqrt(num)+1):
-                is_divisor = num%d==0
-
-                if is_divisor and d in min_idxMap:
-                    best_divisor = min(best_divisor,min_idxMap[d])
-                other_divisor = num//d
-                if other_divisor!=d:
-                    is_divisor = num%other_divisor==0
-                    if is_divisor and other_divisor in min_idxMap:
-                        best_divisor = min(best_divisor,min_idxMap[other_divisor])
-
-            return best_divisor if best_divisor!=float('inf') else -1
-
-        for idx,group in enumerate(groups):
-            res[idx]=helper(group)
-
+        for group in groups:
+            best = float('inf')
+            for d in range(1, math.isqrt(group) + 1):
+                if group % d == 0:
+                    if d in min_idxMap:
+                        best = min(best, min_idxMap[d])
+                    other = group // d
+                    if other != d and other in min_idxMap:
+                        best = min(best, min_idxMap[other])
+            res.append(best if best != float('inf') else -1)
         return res
 
-solution = Solution()
-
+solution=Solution()
 print(solution.assignElements([2,1,2], [1,2,3,4,5]))  # [2, 1, 4, 3, 5]
